2e6c445a834718dde46217d1bdb30076915edfff
Alí <ali_escamilla@epam.com>
2024-07-18 11:33:27 -0600

    test

--- src/Api/Controllers/WeatherForecastController.cs
+++ src/Api/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Me"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
